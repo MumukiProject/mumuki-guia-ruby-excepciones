0f9bb5118b702163a36a0a687a36ff1c0b3be412
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2017-12-18 15:19:09 -0500

    Mumuki Export on 2017-12-18 15:19:09 -0500

--- 00006_Abortando la evaluación/test.rb
+++ 00006_Abortando la evaluación/test.rb
@@ -1,7 +1,7 @@
 it "si Pepita vuela una vez, consume 20" do
   Pepita.volar_en_circulos!
   expect(Pepita.energia).to eq 30
-}
+end
 
 
 it "si Pepita vuela dos veces, consume 40" do
@@ -9,15 +9,15 @@ it "si Pepita vuela dos veces, consume 40" do
   Pepita.volar_en_circulos!
 
   expect(Pepita.energia).to eq 10
-}
+end
 
 it "si le pedimos a Pepita que vuele 3 veces, lanza una excepción y consume sólo 40" do
   2.times { Pepita.volar_en_circulos! }
   expect { Pepita.volar_en_circulos! }.to raise_error
   expect(Pepita.energia).to eq 10
-}
+end
 
 it "La excepción que se lanza tiene la descripción correcta" do
   2.times { Pepita.volar_en_circulos! }
   expect { Pepita.volar_en_circulos! }.to raise_error("No tengo suficiente energía")
-}
+end
