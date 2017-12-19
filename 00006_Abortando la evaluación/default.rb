0bacf84f7fe6f48533cc34f4935a6a75924a3cdc
agus@mumuki.org <agus@mumuki.org>
2017-12-19 14:03:52 -0500

    Mumuki Export on 2017-12-19 14:03:52 -0500

--- 00006_Abortando la evaluación/default.rb
+++ 00006_Abortando la evaluación/default.rb
@@ -1,14 +1,14 @@
 module Pepita
   @energia = 50
   
-  method volar_en_circulos!
+  def self.volar_en_circulos!
     # modificá este método...
-    if @energia > 20
+    if @energia >= 20
        @energia -= 20
     end
   end
   
-  def energia
+  def self.energia
     @energia
   end
 end
