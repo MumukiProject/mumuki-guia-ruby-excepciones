611220e5fe9f6e36de0cd2ef3a2bfe78596bad98
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-19 10:05:55 -0500

    Mumuki Export on 2018-01-19 10:05:55 -0500

--- 00007_Propagación/test.rb
+++ 00007_Propagación/test.rb
@@ -1,11 +1,11 @@
 transferencia = Transferencia.new(40)
 
 it "Realizar una transferencia de la cuenta origen a la destino falla" do
-  expect { transferencia.realizar! }. to raise_error
+  expect { transferencia.realizar!(cuenta_origen, cuenta_destino) }. to raise_error
 end
 
 it "Deshacer una transferencia de la cuenta destino a la origen las deja con los saldos correctos" do
-  transferencia.deshacer!
+  transferencia.deshacer!(cuenta_origen, cuenta_destino)
   expect(cuenta_origen.saldo).to eq 60 
   expect(cuenta_destino.saldo).to eq 60
 end
