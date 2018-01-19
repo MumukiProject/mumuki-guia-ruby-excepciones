1f50b4d91c18690b35706ff782bd5e33768d892d
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-19 09:08:23 -0500

    Mumuki Export on 2018-01-19 09:08:23 -0500

--- 00002_Sólo Volar Si.../test.rb
+++ 00002_Sólo Volar Si.../test.rb
@@ -1,17 +1,17 @@
 describe do 
   it "si Pepita vuela una vez, consume 20" do
-    Pepita.volar_en_circulos!
-    expect(Pepita.energia).to eq 30
+    pepita.volar_en_circulos!
+    expect(pepita.energia).to eq 30
   end
   
   it "si Pepita vuela dos veces, consume 40" do
-    Pepita.volar_en_circulos!
-    Pepita.volar_en_circulos!
-    expect(Pepita.energia).to eq 10
+    pepita.volar_en_circulos!
+    pepita.volar_en_circulos!
+    expect(pepita.energia).to eq 10
   end
   
   it "si le pedimos a Pepita que vuele 10 veces, consume solo 40" do
-    10.times { Pepita.volar_en_circulos! }
-    expect(Pepita.energia).to eq 10
+    10.times { pepita.volar_en_circulos! }
+    expect(pepita.energia).to eq 10
   end
 end
