6ca423da94334ce663d73543c404928aab08f7dd
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-19 09:39:18 -0500

    Mumuki Export on 2018-01-19 09:39:18 -0500

--- 00006_Abortando la evaluación/test.rb
+++ 00006_Abortando la evaluación/test.rb
@@ -11,12 +11,10 @@ it "Si una golondrina vuela dos veces, consume 40" do
 end
 
 it "Si le pedimos a una golondrina que vuele 3 veces, lanza una excepción y consume sólo 40" do
-  2.times { pepita.volar_en_circulos! }
   expect { pepita.volar_en_circulos! }.to raise_error
   expect(pepita.energia).to eq 10
 end
 
 it "La excepción que se lanza tiene la descripción correcta" do
-  2.times { pepita.volar_en_circulos! }
   expect { pepita.volar_en_circulos! }.to raise_error("No tengo suficiente energía")
 end
