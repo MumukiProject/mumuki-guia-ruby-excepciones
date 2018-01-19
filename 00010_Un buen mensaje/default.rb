4508e2ecbfff126436dd27c5b6dbaf69913d5b28
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-19 10:30:50 -0500

    Mumuki Export on 2018-01-19 10:30:50 -0500

--- 00010_Un buen mensaje/default.rb
+++ 00010_Un buen mensaje/default.rb
@@ -1,15 +1,16 @@
-module Pepita
-  @energia = 50
-  
-  def comer_alpiste!(cantidad)
-    @energia += cantidad * 2
+class Golondrina
+  def initialize
+    @energia = 50
   end
   
-  def comerAlpiste(cantidad) {
+  def energia
+    @energia
+  end
+  
+  def comer_alpiste!(cantidad)
     if cantidad <= 0
       raise "error!"
     end
     @energia += cantidad * 2
   end
 end
-
--- 00010_Un buen mensaje/test.rb
+++ 00010_Un buen mensaje/test.rb
@@ -1,3 +1,5 @@
+pepita = Golondrina.new
+
 it "Pepita.comer_alpiste! 0 debería fallar" do
   expect { Pepita.comer_alpiste! 0 }.to raise_error
 end
@@ -7,13 +9,12 @@ it "Pepita.comer_alpiste! -10 debería fallar" do
 end
 
 it "Pepita.comer_alpiste! -10 debería lanzar un mensaje de error expresivo" do
- begin
-   Pepita.comer_alpiste! -10
- rescue => e
-   expect(["positiv", "negativ", "cantidad", "menor", "cero", "0", "-10"].any? { |it| e.message.include?(it) }).to be true
- end
+  begin
+    Pepita.comer_alpiste! -10
+  rescue => e
+    expect(["positiv", "negativ", "cantidad", "menor", "cero", "0", "-10"].any? { |it| e.message.include?(it) }).to be true
+  end
 end
-
 
 it "Pepita.comer_alpiste! 10 NO debería fallar" do
   Pepita.comer_alpiste! 10
