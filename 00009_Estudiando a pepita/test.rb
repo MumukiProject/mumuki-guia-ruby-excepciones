0cda0c0d062a1384d7f2d9c6d3e11b8883e24984
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-19 10:24:11 -0500

    Mumuki Export on 2018-01-19 10:24:11 -0500

--- 00009_Estudiando a pepita/test.rb
+++ 00009_Estudiando a pepita/test.rb
@@ -1,2 +1,6 @@
-carlos = Ornitologo.new
 
+it "Si pepita tiene poca energía, estudiar a pepita falla" do
+   pepita.volar_en_circulos!
+   pepita.volar_en_circulos!
+   expect { carlos.estudiar_pepita! }.to raise_error
+end
