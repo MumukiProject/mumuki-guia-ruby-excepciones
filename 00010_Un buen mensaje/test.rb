94a5c6d9504508d25333645540920175d7648bd7
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-19 10:31:32 -0500

    Mumuki Export on 2018-01-19 10:31:32 -0500

--- 00010_Un buen mensaje/test.rb
+++ 00010_Un buen mensaje/test.rb
@@ -1,21 +1,21 @@
 pepita = Golondrina.new
 
-it "Pepita.comer_alpiste! 0 debería fallar" do
-  expect { Pepita.comer_alpiste! 0 }.to raise_error
+it "comer_alpiste! 0 debería fallar" do
+  expect { pepita.comer_alpiste! 0 }.to raise_error
 end
 
-it "Pepita.comer_alpiste! -10 debería fallar" do
-  expect { Pepita.comer_alpiste! -10 }.to raise_error
+it "comer_alpiste! -10 debería fallar" do
+  expect { pepita.comer_alpiste! -10 }.to raise_error
 end
 
-it "Pepita.comer_alpiste! -10 debería lanzar un mensaje de error expresivo" do
+it "comer_alpiste! -10 debería lanzar un mensaje de error expresivo" do
   begin
-    Pepita.comer_alpiste! -10
+    pepita.comer_alpiste! -10
   rescue => e
     expect(["positiv", "negativ", "cantidad", "menor", "cero", "0", "-10"].any? { |it| e.message.include?(it) }).to be true
   end
 end
 
-it "Pepita.comer_alpiste! 10 NO debería fallar" do
-  Pepita.comer_alpiste! 10
+it "comer_alpiste! 10 NO debería fallar" do
+  pepita.comer_alpiste! 10
 end
