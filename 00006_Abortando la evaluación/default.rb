826f39c5559d000dc39e5ca2fb339e6fda5a65e9
gus.trucco@gmail.com <gus.trucco@gmail.com>
2018-08-07 10:23:07 -0400

    Mumuki Export on 2018-08-07 10:23:07 -0400

--- 00006_Abortando la evaluación/default.rb
+++ 00006_Abortando la evaluación/default.rb
@@ -1,14 +1,1 @@
-class Golondrina
-  def initialize
-    @energia = 50
-  end
-  
-  def energia
-    @energia
-  end
-  
-  def volar_en_circulos!
-    #Modificá este método...
-    @energia -= 20
-  end
-end
+#...solution[2]...#
