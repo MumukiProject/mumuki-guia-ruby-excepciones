37ccd68bfd648ae3b71843cd51ab709dc258f431
franco@mumuki.org <franco@mumuki.org>
2020-11-24 21:51:32 +0000

    Mumuki Export on 2020-11-24 21:51:32 +0000

--- 00009_Estudiando a pepita/test.rb
+++ 00009_Estudiando a pepita/test.rb
@@ -1,7 +1,7 @@
 carlos = Ornitologo.new
 
 it "Si pepita tiene poca energía, estudiar a pepita falla" do
-  pepita.volar_en_circulos!
-  pepita.volar_en_circulos!
+  Pepita.volar_en_circulos!
+  Pepita.volar_en_circulos!
   expect { carlos.estudiar_pepita! }.to raise_error
 end
